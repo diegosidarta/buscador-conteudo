312eabb365ac99d54a4bed3a89115b03c7bb9d5f
Diego Sidarta <diegosidarta123@gmail.com>
2026-09-19 21:51:40 -0300

    estrutura inicial do projeto Spring Boot

---
+++ buscador-conteudo/src/main/java/com/diegosidarta/buscador/BuscadorConteudoApplication.java
@@ -0,0 +1,13 @@
+package com.diegosidarta.buscador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscadorConteudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscadorConteudoApplication.class, args);
+	}
+
+}
---
+++ buscador-conteudo/src/test/java/com/diegosidarta/buscador/BuscadorConteudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegosidarta.buscador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorConteudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
